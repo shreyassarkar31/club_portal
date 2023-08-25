45927264b456aa178563c45ce3b89e1f75d04f24
shreyassarkar31 <71889007+shreyassarkar31@users.noreply.github.com>
2023-08-25 21:49:26 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect('mongodb+srv://31shreyassarkar13:qquZWE0wmpVZeWB2@cluster0.50wryra.mongodb.net/MusicPlayerUsers')
+
+
+const notesSchema = {
+
+    Firstname: String,
+    Lastname: String
+}
+
+const Note = mongoose.model("Note", notesSchema);
+
+
+app.get("/", function(req, res) {
+
+    res.sendFile(__dirname + "/index.html")
+})
+
+
+app.post("/", function(req, res) {
+
+    let newNote = new Note({
+        Firstname: req.body.Firstname,
+        Lastname: req.body.Lastname
+    })
+    newNote.save();
+    res.redirect('/');
+})
+
+app.listen(3000, function() {
+    console.log("server is running on 3000")
+})
+

75872ac33c067b4b842ec207498fb62c4f8b3e6c
shreyassarkar31 <71889007+shreyassarkar31@users.noreply.github.com>
2023-08-30 13:47:18 +0530

    js file for fetch data

---
+++ userlist.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const ejs = require('ejs');
+const { kStringMaxLength } = require('buffer');
+
+app.set('view engine', 'ejs');
+
+mongoose.connect('mongodb+srv://31shreyassarkar13:qquZWE0wmpVZeWB2@cluster0.50wryra.mongodb.net/MusicPlayerUsers');
+
+const userSchema = {
+    Name: String,
+    password: String,
+    Email: String
+}
+
+const sch = mongoose.model('notes', userSchema);
+
+app.get('/', (req, res) => {
+    sch.find({}, function(err, notes) {
+        res.render('index', {
+            usersList: notes
+        })
+    })
+})
+
+app.listen(4000, function() {
+    console.log('server is running');
+})
